85c03b1b3aa38de28db6d0daf2388a4faa82af01
Jorge Camargo <practicas.0016@sesperu.onmicrosoft.com>
2023-07-19 23:20:30 -0500

    subiendo front v1.0

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  name: 'dev',
+  firebase: {
+    config: {
+      apiKey: "AIzaSyC6z1BX-zOu46Vec-1QUyGMyhG75zwjmLU",
+      authDomain: "edificacion-app-4e9f8.firebaseapp.com",
+      projectId: "edificacion-app-4e9f8",
+      storageBucket: "edificacion-app-4e9f8.appspot.com",
+      messagingSenderId: "909867236526",
+      appId: "1:909867236526:web:6ba3307972aba8bc37413f",
+      measurementId: "G-CMLC8GCY21"
+    }
+  }
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  name: 'prod',
+  firebase: {
+    config: {
+      apiKey: "AIzaSyC6z1BX-zOu46Vec-1QUyGMyhG75zwjmLU",
+      authDomain: "edificacion-app-4e9f8.firebaseapp.com",
+      projectId: "edificacion-app-4e9f8",
+      storageBucket: "edificacion-app-4e9f8.appspot.com",
+      messagingSenderId: "909867236526",
+      appId: "1:909867236526:web:6ba3307972aba8bc37413f",
+      measurementId: "G-CMLC8GCY21"
+    }
+  }
+};
